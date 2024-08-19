7445ef9319c3527f86bdfc192645374551654a36
Carolina Ribeiro <89712311+CarolinaRibeiro790@users.noreply.github.com>
2024-08-19 17:04:17 -0300

    .

--- mobile/src/components/button.tsx
+++ mobile/src/components/button.tsx
@@ -1,4 +1,5 @@
-import { Text, TextProps, TouchableOpacity, TouchableOpacityProps } from "react-native";
+import { createContext, useContext } from "react";
+import { Text, TextProps, TouchableOpacity, TouchableOpacityProps, ActivityIndicator} from "react-native";
 import clsx from "clsx"
 
 type Variants = "primary" | "secondary"
@@ -8,21 +9,47 @@ type ButtonProsps = TouchableOpacityProps & {
     isLoading?: boolean
 }
 
-function Button({variant = "primary", children, isLoading, ...rest}: ButtonProsps){
+const ThemeContext = createContext<{variant?: Variants}> ({})
+
+function Button({
+    variant = "primary",
+    children, 
+    isLoading,
+    className, 
+    ...rest
+}: ButtonProsps){
     return ( 
-    <TouchableOpacity className={clsx("w-full h-11 flex-row items-center justify-center rounded-lg gap-2", 
+    <TouchableOpacity
+    className={clsx(
+        "h-11 flex-row items-center justify-center rounded-lg gap-2 px-2", 
         {
             "bg-lime-300": variant === "primary",
             "bg-zinc-800": variant === "secondary",
-        }
-    )} {...rest}>
-        {children}
+        },
+        className
+    )} 
+    activeOpacity={0.7}
+    disabled={isLoading}
+        {...rest}
+    >
+        <ThemeContext.Provider value={{variant}}>
+            {isLoading ? <ActivityIndicator className="text-lime-950"/> : children}
+        </ThemeContext.Provider>
+       
     </TouchableOpacity>
     )
 }
 
 function Title({children}: TextProps){
-    return <Text>{children}</Text>
+    const { variant } = useContext(ThemeContext)
+    return (
+    <Text 
+        className = {clsx("text-base font-semibold",
+        {"text-lime-950": variant === "primary", 
+        "text-zinc-200" : variant === "secondary"})}>
+        {children}
+    </Text>
+    )
 }
 
 Button.Title = Title
---
+++ mobile/src/styles/cores.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from "nativewind";
+
+export const styles = StyleSheet.create({
+    btnVerde: {
+        backgroundColor: '#BEF264', 
+        borderRadius:5, 
+        height: 45, 
+        alignItems:'center', 
+        justifyContent: 'center',
+        flexDirection: 'row'
+    },
+    btnCinza:{
+        backgroundColor: '#27272A', 
+        borderRadius:5, 
+        height: 45, 
+        alignItems:'center', 
+        justifyContent: 'center', 
+        flexDirection: 'row'
+    }
+})
